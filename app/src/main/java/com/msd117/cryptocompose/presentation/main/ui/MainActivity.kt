4bf7c6489b81576186ad5fe2376d9d7e5e61eec1
msd117c <msd117steam@gmail.com>
2021-06-13 17:31:42 +0200

    refactor: Define package for main activity. Include the activity in dagger graph.
    Updates AndroidManifest according to MainActivity new package. Update xml file according to MainActivity new package.

--- app/src/main/java/com/msd117/cryptocompose/presentation/main/ui/MainActivity.kt
+++ app/src/main/java/com/msd117/cryptocompose/presentation/main/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.msd117.cryptocompose
+package com.msd117.cryptocompose.presentation.main.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.msd117.cryptocompose.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
