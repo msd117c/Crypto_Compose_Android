6304c28e2cb895e92e93f1bf93ebcecb405483dd
msd117c <msd117c@gmail.com>
2022-01-16 20:00:20 +0100

    [feat]: implement shimmer loading view

--- app/src/main/java/com/msd117/cryptocompose/presentation/latest/ui/LatestCoinLoadingView.kt
+++ app/src/main/java/com/msd117/cryptocompose/presentation/latest/ui/LatestCoinLoadingView.kt
@@ -1,19 +1,96 @@
 package com.msd117.cryptocompose.presentation.latest.ui
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.msd117.cryptocompose.theme.BaseView
+import com.valentinilk.shimmer.shimmer
 
 @Composable
 fun LatestCoinLoadingView() {
-    Box(modifier = Modifier.fillMaxSize()) {
-        CircularProgressIndicator(
-            modifier = Modifier.align(Alignment.Center),
-            color = MaterialTheme.colors.onPrimary
-        )
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        (0..10).forEach { _ ->
+            item { LoadingItemView() }
+        }
+    }
+}
+
+@Composable
+fun LoadingItemView() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp, 4.dp)
+            .shadow(4.dp)
+    ) {
+        Box(modifier = Modifier.fillMaxWidth()) {
+            Row {
+                Box(
+                    modifier = Modifier
+                        .padding(12.dp, 4.dp, 0.dp, 4.dp)
+                        .requiredSize(52.dp)
+                        .align(Alignment.CenterVertically)
+                        .clip(CircleShape)
+                        .shimmer()
+                        .background(color = Color.Gray),
+                )
+                Column(modifier = Modifier.align(Alignment.CenterVertically)) {
+                    Box(
+                        modifier = Modifier
+                            .padding(8.dp, 2.dp)
+                            .width(70.dp)
+                            .height(18.dp)
+                            .shimmer()
+                            .background(color = Color.Gray)
+                    )
+                    Box(
+                        modifier = Modifier
+                            .padding(8.dp, 2.dp)
+                            .width(50.dp)
+                            .height(18.dp)
+                            .shimmer()
+                            .background(color = Color.Gray)
+                    )
+                }
+            }
+            Column(modifier = Modifier.align(Alignment.CenterEnd)) {
+                Box(
+                    modifier = Modifier
+                        .padding(8.dp, 2.dp)
+                        .width(90.dp)
+                        .height(18.dp)
+                        .shimmer()
+                        .background(color = Color.Gray)
+                        .align(Alignment.End)
+                )
+                Box(
+                    modifier = Modifier
+                        .padding(8.dp, 2.dp)
+                        .width(50.dp)
+                        .height(18.dp)
+                        .shimmer()
+                        .background(color = Color.Gray)
+                        .align(Alignment.End)
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LatestCoinLoadingPreview() {
+    BaseView {
+        LatestCoinLoadingView()
     }
 }
