c3674cc192445269684b50a9494258ecaa8d20eb
msd117c <msd117c@gmail.com>
2022-01-17 19:50:53 +0100

    [feat]: implement loading view components

--- app/src/main/java/com/msd117/cryptocompose/presentation/latest/ui/LatestCoinLoadingView.kt
+++ app/src/main/java/com/msd117/cryptocompose/presentation/latest/ui/LatestCoinLoadingView.kt
@@ -1,20 +1,18 @@
 package com.msd117.cryptocompose.presentation.latest.ui
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.msd117.cryptocompose.theme.BaseView
-import com.valentinilk.shimmer.shimmer
+import com.msd117.cryptocompose.theme.ui.Height
+import com.msd117.cryptocompose.theme.ui.LoadingCircle
+import com.msd117.cryptocompose.theme.ui.LoadingText
 
 @Composable
 fun LatestCoinLoadingView() {
@@ -35,51 +33,38 @@ fun LoadingItemView() {
     ) {
         Box(modifier = Modifier.fillMaxWidth()) {
             Row {
-                Box(
+                LoadingCircle(
+                    size = 52.dp,
                     modifier = Modifier
                         .padding(12.dp, 4.dp, 0.dp, 4.dp)
-                        .requiredSize(52.dp)
                         .align(Alignment.CenterVertically)
-                        .clip(CircleShape)
-                        .shimmer()
-                        .background(color = Color.Gray),
                 )
                 Column(modifier = Modifier.align(Alignment.CenterVertically)) {
-                    Box(
-                        modifier = Modifier
-                            .padding(8.dp, 2.dp)
-                            .width(70.dp)
-                            .height(18.dp)
-                            .shimmer()
-                            .background(color = Color.Gray)
+                    LoadingText(
+                        width = 70.dp,
+                        height = Height.Medium,
+                        modifier = Modifier.padding(8.dp, 2.dp)
                     )
-                    Box(
-                        modifier = Modifier
-                            .padding(8.dp, 2.dp)
-                            .width(50.dp)
-                            .height(18.dp)
-                            .shimmer()
-                            .background(color = Color.Gray)
+                    LoadingText(
+                        width = 50.dp,
+                        height = Height.Medium,
+                        modifier = Modifier.padding(8.dp, 2.dp)
                     )
                 }
             }
             Column(modifier = Modifier.align(Alignment.CenterEnd)) {
-                Box(
+                LoadingText(
+                    width = 90.dp,
+                    height = Height.Medium,
                     modifier = Modifier
                         .padding(8.dp, 2.dp)
-                        .width(90.dp)
-                        .height(18.dp)
-                        .shimmer()
-                        .background(color = Color.Gray)
                         .align(Alignment.End)
                 )
-                Box(
+                LoadingText(
+                    width = 50.dp,
+                    height = Height.Medium,
                     modifier = Modifier
                         .padding(8.dp, 2.dp)
-                        .width(50.dp)
-                        .height(18.dp)
-                        .shimmer()
-                        .background(color = Color.Gray)
                         .align(Alignment.End)
                 )
             }
