9062e45c60411254491866d065ea1290f8e6505a
Miguel Sánchez <miguel.sanchez@xing.com>
2022-11-09 14:18:01 +0100

    fix latest coins response and implement categories loading view

--- app/src/main/java/com/msd117/cryptocompose/latest/data/model/LatestCoinsResponse.kt
+++ app/src/main/java/com/msd117/cryptocompose/latest/data/model/LatestCoinsResponse.kt
@@ -1,6 +1,9 @@
 package com.msd117.cryptocompose.latest.data.model
 
+import com.squareup.moshi.Json
+
 data class LatestCoinsResponse(
+    @Json(name = "data")
     val coinDataList: List<CoinData>?,
     val status: Status?
 )
