ec12a127f34e311e1e3837444b296818e04bd527
Miguel Sánchez <miguel.sanchez@xing.com>
2022-11-10 12:40:10 +0100

    Improve main view performance

--- app/src/main/java/com/msd117/cryptocompose/main/presenter/MainState.kt
+++ app/src/main/java/com/msd117/cryptocompose/main/presenter/MainState.kt
@@ -1,11 +1,24 @@
 package com.msd117.cryptocompose.main.presenter
 
+import androidx.annotation.StringRes
+import com.msd117.cryptocompose.R
+import com.msd117.cryptocompose.utils.NavigationConstants.CategoriesRoute
+import com.msd117.cryptocompose.utils.NavigationConstants.LatestCoinsRoute
 import com.msd117.cryptocompose.utils.State
 
 sealed class MainState : State {
 
     object Uninitialized : MainState()
-    data class Loaded(val isConnected: Boolean) : MainState()
+    data class Loaded(
+        val isConnected: Boolean,
+        val items: List<MenuItem> = MenuItem.values().toList()
+    ) : MainState() {
+
+        enum class MenuItem(@StringRes val label: Int, val route: String) {
+            LATEST_COINS(R.string.latest_coins_title, LatestCoinsRoute),
+            CATEGORIES(R.string.menu_item_categories, CategoriesRoute),
+        }
+    }
 }
 
 val initialState = MainState.Uninitialized
--- app/src/main/java/com/msd117/cryptocompose/main/ui/MainView.kt
+++ app/src/main/java/com/msd117/cryptocompose/main/ui/MainView.kt
@@ -4,6 +4,7 @@ import androidx.annotation.StringRes
 import androidx.compose.animation.Crossfade
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
@@ -25,8 +26,6 @@ import com.msd117.cryptocompose.theme.Padding.paddingM
 import com.msd117.cryptocompose.theme.Padding.paddingS
 import com.msd117.cryptocompose.theme.Size.sizeS
 import com.msd117.cryptocompose.theme.ui.widget.BodyText
-import com.msd117.cryptocompose.utils.NavigationConstants.CategoriesRoute
-import com.msd117.cryptocompose.utils.NavigationConstants.LatestCoinsRoute
 import com.msd117.cryptocompose.utils.NavigationEvent.Route
 
 @Composable
@@ -38,38 +37,31 @@ fun MainView(viewModel: MainViewModel) {
             is MainState.Uninitialized -> Unit
             is MainState.Loaded -> {
                 Box(modifier = Modifier.fillMaxSize()) {
-                    Column(modifier = Modifier.fillMaxWidth()) {
-                        MainMenuItem(
-                            label = R.string.menu_item_coins,
-                            isConnected = state.isConnected,
-                            onClicked = { viewModel.navigate(Route(LatestCoinsRoute)) }
-                        )
-                        MainMenuItem(
-                            label = R.string.menu_item_categories,
-                            isConnected = state.isConnected,
-                            onClicked = { viewModel.navigate(Route(CategoriesRoute)) }
-                        )
-                        MainMenuItem(
-                            label = R.string.menu_item_markets,
-                            isConnected = state.isConnected,
-                            onClicked = {}
-                        )
-                    }
-                    if (!state.isConnected) {
-                        Card(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .align(Alignment.BottomCenter)
-                                .padding(horizontal = paddingM, vertical = paddingS)
-                                .shadow(elevation = sizeS),
-                            backgroundColor = MaterialTheme.colors.error
-                        ) {
-                            BodyText(
-                                text = stringResource(R.string.no_connection_message),
-                                color = MaterialTheme.colors.onError,
-                                modifier = Modifier.padding(all = paddingL)
-                            )
-                        }
+                    MainMenuView(
+                        state = state,
+                        onClicked = { route -> viewModel.navigate(Route(route)) }
+                    )
+                    MainConnectionErrorView(isConnectedProvider = state::isConnected)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun MainMenuView(state: MainState.Loaded, onClicked: (String) -> Unit) {
+    LazyColumn {
+        with(state.items) {
+            items(
+                count = size,
+                key = { index -> get(index).route }
+            ) { index ->
+                with(get(index)) {
+                    MainMenuItem(
+                        labelProvider = ::label,
+                        isConnectedProvider = state::isConnected
+                    ) {
+                        onClicked(route)
                     }
                 }
             }
@@ -78,9 +70,9 @@ fun MainView(viewModel: MainViewModel) {
 }
 
 @Composable
-fun MainMenuItem(
-    @StringRes label: Int,
-    isConnected: Boolean,
+private fun MainMenuItem(
+    @StringRes labelProvider: () -> Int,
+    isConnectedProvider: () -> Boolean,
     onClicked: () -> Unit
 ) {
     Card(
@@ -89,10 +81,30 @@ fun MainMenuItem(
             .padding(all = paddingM)
             .shadow(elevation = sizeS)
     ) {
-        Button(onClick = { onClicked() }, enabled = isConnected) {
+        Button(onClick = { onClicked() }, enabled = isConnectedProvider()) {
             BodyText(
-                text = stringResource(label),
+                text = stringResource(labelProvider()),
                 modifier = Modifier.padding(horizontal = paddingM, vertical = paddingL)
+            )
+        }
+    }
+}
+
+@Composable
+private fun BoxScope.MainConnectionErrorView(isConnectedProvider: () -> Boolean) {
+    if (!isConnectedProvider()) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .padding(horizontal = paddingM, vertical = paddingS)
+                .shadow(elevation = sizeS),
+            backgroundColor = MaterialTheme.colors.error
+        ) {
+            BodyText(
+                text = stringResource(R.string.no_connection_message),
+                color = MaterialTheme.colors.onError,
+                modifier = Modifier.padding(all = paddingL)
             )
         }
     }
